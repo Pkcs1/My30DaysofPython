807fe9c49a8e515165a3895a5fcbc11caeb0a80b
Pkcs1 <ramaoctantyo@gmail.com>
2026-06-26 19:08:19 +0700

    Update Day11lvl3.py

--- Day11-20/Day11/Day11lvl3.py
+++ Day11-20/Day11/Day11lvl3.py
@@ -1,11 +1,9 @@
-"""#1
+# 1
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2,n):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
-
-print(is_prime(7))"""
-#2
+print(is_prime(7))
